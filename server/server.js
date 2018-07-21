6d33d526cd851f02b79ae80c68eb9f1b1019ec67
stvyzxx <yurasobechko@gmail.com>
2018-07-21 17:33:58 +0300

    changed port to uppercase

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const { ObjectID } = require('mongodb');
 
 const { Todo } = require('./models/todo');
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 
