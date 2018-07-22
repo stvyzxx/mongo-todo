e1f253d5e23ad4ee692c5815c15dc21d3ecabb89
stvyzxx <yurasobechko@gmail.com>
2018-07-22 11:42:20 +0300

    finished crud

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
 const { ObjectID } = require('mongodb');
 
 const { Todo } = require('./models/todo');
@@ -69,6 +70,32 @@ app.delete('/todos/:id', (req, res) => {
   }, e => {
     res.status(404).send(e);
   });
+});
+
+app.patch('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  const body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
 });
 
 app.listen(port, () => {
