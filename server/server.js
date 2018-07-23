965af3faed682d62ee052fa22544f7c72a05fbc5
stvyzxx <yurasobechko@gmail.com>
2018-07-24 01:24:23 +0300

    added auth

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const _ = require('lodash');
 const { ObjectID } = require('mongodb');
 
 const { Todo } = require('./models/todo');
+const { User } = require('./models/user');
 const { mongoose } = require('./db/mongoose');
 
 const port = process.env.PORT || 3000;
@@ -96,6 +97,21 @@ app.patch('/todos/:id', (req, res) => {
   }).catch((e) => {
     res.status(400).send();
   })
+});
+
+app.post('/users', (req, res) => {
+  const body = _.pick(req.body, ['email', 'password']);
+
+  const user = new User(body);
+
+  user.save()
+    .then(() => user.generateAuthToken())
+    .then(token => {
+      res.header('x-auth', token).send(user)
+    })
+    .catch(e => {
+      res.status(400).send(e);
+    });
 });
 
 app.listen(port, () => {
