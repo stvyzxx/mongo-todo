fe90349f2d37c6e9d8459f4ae08b85b9465d1e7f
stvyzxx <yurasobechko@gmail.com>
2018-07-25 01:19:31 +0300

    password hashing

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
   email: {
@@ -32,6 +33,21 @@ const UserSchema = new mongoose.Schema({
   }]
 });
 
+UserSchema.pre('save', function (next) {
+  const user = this;
+
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
 UserSchema.methods.toJSON = function() {
   const user = this;
   const userObject = user.toObject();
@@ -47,6 +63,27 @@ UserSchema.methods.generateAuthToken = function() {
   user.tokens = user.tokens.concat([{access, token}]);
 
   return user.save().then(() => token);
+}
+
+UserSchema.statics.findByCredentials = function(email, password) {
+  const User = this;
+
+  return User.findOne({ email })
+    .then(user => {
+      if (!user) {
+        return Promise.reject();
+      }
+
+      return new Promise((resolve, reject) => {
+        bcrypt.compare(password, user.password, (err, res) => {
+          if (res) {
+            resolve(user);
+          } else {
+            reject(err);
+          }
+        });
+      });
+    })
 }
 
 UserSchema.statics.findByToken = function(token) {
